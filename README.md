59f02cf3ffada94934e12e7fbc6a4a26ab56d75e
Min Yong An <minyong@Mins-MacBook-Air.local>
2024-08-14 18:01:25 +0900

    python renewal

--- README.md
+++
@@ -1,5 +0,0 @@
-# CIAE
-
-### Langauge = Go (https://go.dev/doc/install)
-### Web Framework = Gin (https://github.com/gin-gonic/gin)
-### Database = PostgreSQL (https://www.postgresql.org/download/)
--- internal/model/lecture.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// description of lecture video
-type lecture struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	VideoURL    string `json:"vid_url"`
-}
