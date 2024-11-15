f8af0fe8a60ccedf9f20f74c6cfad03b7ab462d0
minyong926 <mdragon2004@gmail.com>
2024-11-16 01:12:15 +0900

    Fixed merge confilcts

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,13 +1,14 @@
 import psycopg2
-from flask import Flask, current_app, g, jsonify, g
+from flask import Flask, current_app, jsonify, g
 from flask_cors import CORS
 from psycopg2.extras import RealDictCursor
 import click
 import os
 
-#DC edited
+# DC edited
 app = Flask(__name__)
 CORS(app)  # CORS 허용
+
 
 def get_db():
     if "db" not in g:
@@ -47,21 +48,24 @@ def init_app(app):
     app.cli.add_command(init_db_command)
 
 
-#DC edited
+# DC edited
 @app.route("/api/books", methods=["GET"])
 def get_books():
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT book_name, book_page_num FROM book")
     books = cursor.fetchall()
-    result = [{"book_name": book[0], "book_page_num": book[1]} for book in books]
+    result = [{"book_name": book[0], "book_page_num": book[1]}
+              for book in books]
     return jsonify(result)
+
 
 @app.route("/api/bookpage", methods=["GET"])
 def get_bookpage():
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("SELECT book_id, page_num, page_text, page_pdf FROM book_page ")
+    cursor.execute(
+        "SELECT book_id, page_num, page_text, page_pdf FROM book_page ")
     pages = cursor.fetchall()
     result = [
         {
@@ -69,44 +73,49 @@ def get_bookpage():
             "page_num": page[1],
             "page_text": page[2],
             "page_pdf": page[3]
-            }
-            for page in pages
+        }
+        for page in pages
     ]
     return jsonify(result)
+
 
 @app.route("/api/chapNum", methods=["GET"])
 def get_chapnum():
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("SELECT chapter_id, book_id, start_page, end_page FROM chapter_page")
+    cursor.execute(
+        "SELECT chapter_id, book_id, start_page, end_page FROM chapter_page")
     chapters = cursor.fetchall()
     result = [
-            {
-                "chapter_id": chapter[0],
-                "book_id": chapter[1],
-                "start_page": chapter[2],
-                "end_page": chapter[3]
-            }
-            for chapter in chapters
-        ]
+        {
+            "chapter_id": chapter[0],
+            "book_id": chapter[1],
+            "start_page": chapter[2],
+            "end_page": chapter[3]
+        }
+        for chapter in chapters
+    ]
     return jsonify(result)
+
 
 @app.route("/api/getScores", methods=["GET"])
 def get_scores():
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("SELECT identification_score, catharsis_score, insight_score, score_total FROM score")
+    cursor.execute(
+        "SELECT identification_score, catharsis_score, insight_score, score_total FROM score")
     scores = cursor.fetchall()
     result = [
-            {
-                "identification_score": type[0],
-                "catharsis_score": type[1],
-                "insight_score": type[2],
-                "score_total": type[3]
-            }
-            for type in scores
-        ]
+        {
+            "identification_score": type[0],
+            "catharsis_score": type[1],
+            "insight_score": type[2],
+            "score_total": type[3]
+        }
+        for type in scores
+    ]
     return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
-import Turtle from './components/Turtle';
-import Learning from './components/Learning';
-import Bookshelf from './components/Bookshelf';
-import Home from './components/Home';
-import Village from './components/Village';
-import Login from './components/Login';
-import DeepSea from './components/TurtleVillage/DeepSea';
-import Space from './components/TurtleVillage/Space';
-import Sky from './components/TurtleVillage/Sky';
-import Forest from './components/TurtleVillage/Forest';
-import Recommend from './components/Recommend';
-import Membership from './components/Membership';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import Turtle from "./components/Turtle";
+import Learning from "./components/Learning";
+import Bookshelf from "./components/Bookshelf";
+import Home from "./components/Home";
+import Village from "./components/Village";
+import Login from "./components/Login";
+import DeepSea from "./components/TurtleVillage/DeepSea";
+import Space from "./components/TurtleVillage/Space";
+import Sky from "./components/TurtleVillage/Sky";
+import Forest from "./components/TurtleVillage/Forest";
+import Recommend from "./components/Recommend";
+import Membership from "./components/Membership";
 
 function List(props) {
   return (
@@ -25,39 +31,52 @@ function List(props) {
 function Logo() {
   return (
     <li>
-      <Link to='/Home'><img className='logo' src='/CIAE로고 2.png' alt='logo' /></Link>
-    </li> 
+      <Link to="/Home">
+        <img className="logo" src="/CIAE로고 2.png" alt="logo" />
+      </Link>
+    </li>
   );
 }
 
 function App() {
   const location = useLocation();
-  
+
   const hideBackgroundPaths = [];
-  const hideNavPaths = ['/Learning', '/Login', '/Recommend', '/Membership'];
+  const hideNavPaths = ["/Learning", "/Login", "/Recommend", "/Membership"];
   const showNav = !hideNavPaths.includes(location.pathname);
   const showBackground = !hideBackgroundPaths.includes(location.pathname);
   return (
     <div>
       {showBackground && (
-        <div className='Main'>
-          <img src='/MainBackground.png' className='MainBackground' alt='MainBackground'/>
+        <div className="Main">
+          <img
+            src="/MainBackground.png"
+            className="MainBackground"
+            alt="MainBackground"
+          />
           {showNav && (
-          <>
-          <audio autoPlay loop>
-            <source src="/The Center Isn't Holding - National Sweetheart.mp3" type="audio/mpeg" />
-          </audio>
-          <nav>
-            <ul className='navAll'>
-              <Logo />
-              <List route='/Bookshelf' name='내 서재' thing='bookshelf' />
-              <List route='/Turtle' name='거북이 미니게임' thing='turtle' />
-              <List route='/Village' name='거북이 마을' thing='village' />
-              <List route='/Recommend' name='독서 시작하기' thing='learning' />
-            </ul>
-          </nav>
-        </> 
-      )}
+            <>
+              <audio autoPlay loop>
+                <source
+                  src="/The Center Isn't Holding - National Sweetheart.mp3"
+                  type="audio/mpeg"
+                />
+              </audio>
+              <nav>
+                <ul className="navAll">
+                  <Logo />
+                  <List route="/Bookshelf" name="내 서재" thing="bookshelf" />
+                  <List route="/Turtle" name="거북이 미니게임" thing="turtle" />
+                  <List route="/Village" name="거북이 마을" thing="village" />
+                  <List
+                    route="/Recommend"
+                    name="독서 시작하기"
+                    thing="learning"
+                  />
+                </ul>
+              </nav>
+            </>
+          )}
         </div>
       )}
       {/* 페이지별 라우팅 설정 */}
@@ -77,13 +96,14 @@ function App() {
         <Route path="/Membership" element={<Membership />} />
       </Routes>
     </div>
-);
+  );
 }
 
 export default function AppWrapper() {
-return (
-  <Router>
-    <App />
-  </Router>
-);
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
 }
+
